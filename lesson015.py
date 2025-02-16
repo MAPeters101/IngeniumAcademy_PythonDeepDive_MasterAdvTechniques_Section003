aaddc0a2a11077c950a1acd09498b7f10c302f46
Mark Peters <mapeters101@earthlink.net>
2025-02-16 18:46:40 -0500

    Work on lesson015 from 1:10 to 1:40.  start_udp_client().

--- lesson015.py
+++ lesson015.py
@@ -18,3 +18,18 @@ def start_udp_server():
 
         # Send a response to the client
         s.sendto(f'Received your message: {client_message.decode()}'.encode(), addr)
+
+def start_udp_client():
+    # Give server a moment to start
+    time.sleep()
+
+    # Create a socket object
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    server_address = ('localhost', 443)
+
+    for i in range(5):
+        # Send a message to the server
+        s.sendto(f'Hello from client, message {i}'.encode(), server_address)
+
+        # Receive the server's response
